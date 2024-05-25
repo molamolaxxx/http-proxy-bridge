1b8cc2577a98c4b8ab0c245b9ab730b207bc17ea
molamolaxxx <408287749@qq.com>
2024-05-26 00:29:52 +0800

    构建正向代理的native image

--- build-native.sh
+++ build-native.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 sh build.sh
 cd ./build
+echo "start build native image"
 # use version : graalvm-jdk-17.0.11+7.1
-native-image --no-fallback -march=compatibility -jar ./http-proxy-reverse.jar http-proxy-reverse
+native-image \
+  --no-fallback \
+  -march=compatibility \
+  -jar ./http-proxy-reverse.jar http-proxy-reverse
+
+native-image \
+  --no-fallback \
+  -march=compatibility \
+  -jar ./http-proxy-forward.jar http-proxy-forward
