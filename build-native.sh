db3ccfa240c352ac75ee3d363fa175349cccf032
molamolaxxx <408287749@qq.com>
2024-07-04 21:01:31 +0800

    aot config update

--- build-native.sh
+++ build-native.sh
@@ -12,3 +12,18 @@ native-image \
   --no-fallback \
   -march=compatibility \
   -jar ./http-proxy-forward.jar http-proxy-forward
+
+# use version : graalvm-ee-java17-21.3.10
+native-image \
+  --no-fallback \
+  --gc=G1 \
+  --allow-incomplete-classpath \
+  --initialize-at-build-time=sun.instrument.InstrumentationImpl \
+  -jar ./http-proxy-reverse.jar http-proxy-reverse
+
+native-image \
+  --no-fallback \
+  --gc=G1 \
+  --allow-incomplete-classpath \
+  --initialize-at-build-time=sun.instrument.InstrumentationImpl \
+  -jar ./http-proxy-forward.jar http-proxy-forward
